6f3a131208b8653a8c4b0f6666167af4bc658e6f
netelli <natalli.dev@gmail.com>
2016-12-15 21:22:51 +0200

    ST-2 - apply review comments.

--- test/com/github/netelli/ProductsDAOTest.java
+++ test/com/github/netelli/ProductsDAOTest.java
@@ -1,27 +1,39 @@
 package com.github.netelli;
 
 import com.github.netelli.model.Product;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ProductsDAOTest {
 
+    private ProductsDAO productsDAO;
+
+    @Before
+    public void setUp() throws Exception {
+        productsDAO = new ProductsDAO("jdbc:h2:mem:test");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        productsDAO.close();
+    }
+
     @Test
     public void testInsertData() throws Exception {
-        ProductsDAO productsDAO = new ProductsDAO("jdbc:h2:mem:test");
         productsDAO.init();
         productsDAO.createTables();
         productsDAO.insertData();
 
         List<Product> products = productsDAO.getProducts();
 
-        assertTrue(products.size() == 3);
+        assertEquals(3, products.size());
+        //TODO fix test
 //        assertEquals("skirt mini", products.get(0));
-        productsDAO.close();
-
     }
 
     @Test(expected = RuntimeException.class)
